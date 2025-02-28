e934f0cb148786672ca198c7e4eb993d80433983
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-02-28 11:40:29 +0000

    initialize project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='font-baij'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatum ipsum vero in accusantium illum esse reiciendis dolorum velit. Error deserunt obcaecati expedita voluptates tempore omnis esse nesciunt nam perferendis.
+    </div>
+  )
+}
+
+export default page
